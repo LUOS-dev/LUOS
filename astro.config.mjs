f91ebea564a35c3f2115eb78ffd4e573bdcdd6a5
Luis Mayorga <eduardo.mayorgab@gmail.com>
2026-02-27 22:41:57 -0600

    arreglo menor

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,17 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
-import node from "@astrojs/node";
+//import vercel from '@astrojs/vercel';
+import vercel from '@astrojs/vercel/serverless';
+//import node from "@astrojs/node";
 
 // https://astro.build/config
 export default defineConfig({
     integrations: [react()],
     site: 'https://misitio.com',
     output: "server",
-    adapter: node({ mode: 'standalone' }),
+    adapter: vercel({
+    webAnalytics: { enabled: true } // Opcional, Vercel tiene buenas métricas
+  }),
+    //adapter: node({ mode: 'standalone' }),
 });
